755b8d025ad1c7cff855d464f8fdf21483cb97b9
Tri Ngo <ngominhtrint@gmail.com>
2018-02-08 00:38:55 +0700

    Display current location.

--- app/src/main/java/sg/tringo/roadbull/MapsActivity.java
+++ app/src/main/java/sg/tringo/roadbull/MapsActivity.java
@@ -1,7 +1,18 @@
 package sg.tringo.roadbull;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,7 +26,42 @@ import sg.tringo.roadbull.views.CustomInfoWindowAdapter;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+    public static final int LOCATION_UPDATE_MIN_DISTANCE = 10;
+    public static final int LOCATION_UPDATE_MIN_TIME = 5000;
+
+    private static final String TAG = "MapsActivity";
+
     private GoogleMap mMap;
+
+    private LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            if (location != null) {
+                Log.d(TAG, String.format("%f, %f", location.getLatitude(), location.getLongitude()));
+                drawMarker(location);
+                mLocationManager.removeUpdates(mLocationListener);
+            }
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
+    private LocationManager mLocationManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,28 +71,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        getCurrentLocation();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getCurrentLocation();
+    }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mLocationManager.removeUpdates(mLocationListener);
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.getUiSettings().setZoomControlsEnabled(true);
 
-        String title = "This is Title";
-        String subTitle = "This is \nSubtitle";
+        String title = "The Coffee House - Hoa Hong";
+        String subTitle = "43 Hoa Hồng, Phường 25, Phú Nhuận, Hồ Chí Minh 700000, Việt Nam";
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        LatLng sydney = new LatLng(10.7957471, 106.6876993);
 
         //Marker
         MarkerOptions markerOpt = new MarkerOptions();
@@ -58,6 +108,56 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         CustomInfoWindowAdapter adapter = new CustomInfoWindowAdapter(MapsActivity.this);
         mMap.setInfoWindowAdapter(adapter);
         mMap.addMarker(markerOpt);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney, 18));
+    }
+
+    private void getCurrentLocation() {
+        boolean isGPSEnabled = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean isNetworkEnabled = mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        Location location = null;
+
+        if (Build.VERSION.SDK_INT >= 23 &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_LOCATION);
+        } else {
+            if (!(isGPSEnabled || isNetworkEnabled)) {
+                Toast.makeText(this, R.string.error_location_provider, Toast.LENGTH_SHORT).show();
+            } else {
+                if (isNetworkEnabled) {
+                    mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                            LOCATION_UPDATE_MIN_TIME, LOCATION_UPDATE_MIN_DISTANCE, mLocationListener);
+                    location = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                }
+
+                if (isGPSEnabled) {
+                    mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                            LOCATION_UPDATE_MIN_TIME, LOCATION_UPDATE_MIN_DISTANCE, mLocationListener);
+                    location = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                }
+            }
+        }
+
+        if (location != null) {
+            Log.d(TAG, String.format("getCurrentLocation(%f, %f)", location.getLatitude(),
+                    location.getLongitude()));
+            drawMarker(location);
+        }
+    }
+
+    private void drawMarker(Location location) {
+        if (mMap != null) {
+            LatLng gps = new LatLng(location.getLatitude(), location.getLongitude());
+            MarkerOptions markerOptions = new MarkerOptions()
+                    .position(gps)
+                    .title("Current location")
+                    .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
+            mMap.addMarker(markerOptions);
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(gps, 18));
+        }
     }
 }
---
+++ app/src/main/java/sg/tringo/roadbull/parser/DataParser.java
@@ -0,0 +1,9 @@
+package sg.tringo.roadbull.parser;
+
+/**
+ * Created by tringo on 2/8/18.
+ */
+
+public class DataParser {
+    
+}
